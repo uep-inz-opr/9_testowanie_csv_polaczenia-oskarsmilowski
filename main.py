1c9ba24aef51505f90c838b6f3c7ca106b9e0456
Oskar <69815@student.ue.poznan.pl>
2022-05-07 22:48:45 +0200

    poprawilem tuple

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ def main():
         reader = csv.DictReader(file)
         for row in reader:
             if row['From'] in slownik_polaczen:
-                slownik_polaczen[int(row['From'])] += 1
+                slownik_polaczen[row['From']] += 1
             else:
-                slownik_polaczen[int(row['From'])] = 1
+                slownik_polaczen[row['From']] = 1
     result_niesortowany = list(slownik_polaczen.items())
     result_posortowany = sorted(result_niesortowany,key=itemgetter(1), reverse=True)
-    print(result_posortowany[0])
+    print(result_posortowany[0][0])
+    result = (int(result_posortowany[0][0]), result_posortowany[0][1])
+    print(result)
 
 if __name__ == "__main__":
     main()
